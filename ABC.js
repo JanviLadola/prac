dfd05b69ab805b7c07cb7ebcafdc6132644841b5
JanviLadola <72727990+JanviLadola@users.noreply.github.com>
2020-10-12 16:39:02 +0530

    initial commit

--- ABC.js
+++ ABC.js
@@ -11,21 +11,18 @@ module.exports = async(req, res) => {
     let getUser = await user.findOne({ Email })
 
     console.log(getUser, "1.1");
-<<<<<<< HEAD
-    // if (getUser != null) {
-    //     return res.status(409).json({
-    //         "data": getUser,
-    //         "message": "already"
-    //     });
-    // }
-=======
     if (getUser != null) {
         return res.status(409).json({
             "data": getUser,
             "message": "already"
         });
     }
->>>>>>> master
+    if (getUser != null) {
+        return res.status(409).json({
+            "data": getUser,
+            "message": "already"
+        });
+    }
     console.log(Email);
     console.log(Password);
     await new user({
